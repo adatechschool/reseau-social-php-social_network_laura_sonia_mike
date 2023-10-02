53d58d05468a28373e9ae5f0ea8d279c11fbe11b
MikeMaille88 <mickael.bohrer@gmail.com>
2023-10-02 15:00:07 +0200

    include header all pages

--- Niveau1/Niveau1/news.php
+++ Niveau1/Niveau1/news.php
@@ -7,32 +7,20 @@
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <header>
-            <a href='admin.php'><img src="resoc.jpg" alt="Logo de notre réseau social"/></a>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">▾ Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-            </nav>
-        </header>
-        <div id="wrapper">
-            <aside>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez les derniers messages de
-                        tous les utilisatrices du site.</p>
-                </section>
-            </aside>
+
+        <?php
+        include 'template.php'
+        ?>
+
+    <div id="wrapper">
+        <aside>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+        <section>
+        <h3>Présentation</h3>
+        <p>Sur cette page vous trouverez les derniers messages de
+            tous les utilisatrices du site.</p>
+        </section>
+        </aside> 
             <main>
                 <!-- L'article qui suit est un exemple pour la présentation et 
                   @todo: doit etre retiré -->
