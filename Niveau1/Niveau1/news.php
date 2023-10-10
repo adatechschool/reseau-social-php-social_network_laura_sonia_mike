5ee0d9a8dd8948675168e71a43bb6318dfc4c590
MikeMaille88 <mickael.bohrer@gmail.com>
2023-10-10 16:28:39 +0200

    update link from name

--- Niveau1/Niveau1/news.php
+++ Niveau1/Niveau1/news.php
@@ -122,7 +122,7 @@
                             <time><?php echo $post['created'] ?></time>
                         </h3>
                         <address>
-                            <a href="wall.php?user_id=<?php echo $userId ?>">
+                            <a href="wall.php?user_id=<?php echo $post['author_id'] ?>">
                             par <?php echo $post['author_name'] ?>
                             </a>
                         </address>
